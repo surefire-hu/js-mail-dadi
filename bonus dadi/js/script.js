1cda7350e8b2903b9719d040eba0de81dc55db29
surefire-hu <surefire.hu2001@gmail.com>
2024-07-16 18:27:22 +0200

    tiro dei dati modo casuale in console

--- bonus dadi/js/script.js
+++ bonus dadi/js/script.js
@@ -1,1 +1,21 @@
+// Funzione per generare un numero casuale da 1 a 6
+function lanciaDado() {
+    return Math.floor(Math.random() * 6) + 1;
+}
 
+// Lancio dei dadi
+let tiroGiocatore = lanciaDado();
+let tiroComputer = lanciaDado();
+
+// Stampa dei risultati
+console.log("Giocatore ha tirato: " + tiroGiocatore);
+console.log("Computer ha tirato: " + tiroComputer);
+
+// Confronto e determinazione del vincitore
+if (tiroGiocatore > tiroComputer) {
+    console.log("Il giocatore vince!");
+} else if (tiroGiocatore < tiroComputer) {
+    console.log("Il computer vince!");
+} else {
+    console.log("È un pareggio!");
+}
