8425b8801c75c30580d1ec292c66409f74c3e575
surefire-hu <surefire.hu2001@gmail.com>
2024-07-16 18:17:59 +0200

    esecuzione console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-
+const emailAutorizzate = [
+    "utente1@scl.com",
+    "utente2@scl.com",
+    "utente3@scl.com",
+    "owner@scl.com",
+    "co-admin@scl.com",
+    "admin@scl.com"
+  ];
+  
+  function controllaAccesso() {
+    // Chiedi all'utente la sua email
+    const emailUtente = prompt("Per favore, inserisci la tua email:");
+  
+    // Controlla se l'email è nella lista delle email autorizzate
+    if (emailAutorizzate.includes(emailUtente)) {
+      console.log("Accesso consentito. Benvenuto!");
+    } else {
+      console.log("Accesso negato. La tua email non è autorizzata.");
+    }
+  }
+  
+  // Esegui la funzione di controllo
+  controllaAccesso();
